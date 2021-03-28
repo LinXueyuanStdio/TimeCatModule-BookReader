98ff1cabd7eb7973d90e2a4f6d188e1798250991
linxueyuanstdio <761516186@qq.com>
2021-03-28 21:05:10 +0800

    Update ReaderContainerService.kt

--- module-book-reader/src/main/java/com/timecat/module/read/ReaderContainerService.kt
+++ module-book-reader/src/main/java/com/timecat/module/read/ReaderContainerService.kt
@@ -1,7 +1,10 @@
 package com.timecat.module.read
 
 import android.content.Context
+import com.google.android.material.chip.Chip
+import com.timecat.component.router.app.NAV
 import com.timecat.identity.readonly.RouterHub
+import com.timecat.layout.ui.layout.setShakelessClickListener
 import com.timecat.middle.block.service.ContainerService
 import com.timecat.middle.block.service.HomeService
 import com.xiaojinzi.component.anno.ServiceAnno
@@ -20,6 +23,18 @@ import kotlinx.coroutines.withContext
  */
 @ServiceAnno(ContainerService::class, name = [RouterHub.GLOBAL_ReaderContainerService])
 class ReaderContainerService : ContainerService {
+    override fun loadContainerButton(context: Context, parentUuid: String, homeService: HomeService, callback: ContainerService.LoadButton) {
+        callback.onLoadSuccess(listOf(
+            Chip(context).apply{
+                text= "书架"
+                setShakelessClickListener {
+                    NAV.go(RouterHub.NOVEL_BookShelfActivity)
+                    homeService.itemCommonListener().close()
+                }
+            }
+        ))
+    }
+
     override fun loadForVirtualPath(context: Context, parentUuid: String, homeService: HomeService, callback: ContainerService.LoadCallback) {
         GlobalScope.launch(Dispatchers.IO) {
             val allBooks = db.bookDao().all
@@ -28,5 +43,9 @@ class ReaderContainerService : ContainerService {
                 callback.onVirtualLoadSuccess(cards)
             }
         }
+    }
+
+    override fun loadMoreForVirtualPath(context: Context, parentUuid: String, offset: Int, homeService: HomeService, callback: ContainerService.LoadMoreCallback) {
+        callback.onVirtualLoadSuccess(listOf())
     }
 }
